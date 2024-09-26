280d6f343a97e5c5246ebfbfd17ef13fb2472f66
Praseeda-mgr <Prasiddhamagar11@gmail.com>
2024-09-27 00:16:29 +0545

    commited

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Danger!!! 'THe world will be finished in september 20'"); 
+// const someObject = { str: "Hello world", id:5};
+// console.log(someObject);
+
+// const Obj = {};
+// console.log(Obj);
+// Obj.prop = 1,2,3;
+// console.log(JSON.parse(JSON.stringify(Obj)));
+
+const Car = {str:"BMW"};
+const someObj = {str: "Salary"};
+console.info("My first car is", Car,". I bought it with my first",someObj,);
+
+// console.time("answer time");
+// alert("Click to Continue");
+// console.timelog("answer time");
+// alert("Do a bunch of other stuffs...");
+// console.timeEnd("answer time");
