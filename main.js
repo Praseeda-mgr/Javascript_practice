508611cdaf3a188083e2d73cff49e6c1ce4d295f
Praseeda-mgr <Prasiddhamagar11@gmail.com>
2024-09-27 13:49:36 +0545

    group console added

--- main.js
+++ main.js
@@ -6,12 +6,23 @@
 // Obj.prop = 1,2,3;
 // console.log(JSON.parse(JSON.stringify(Obj)));
 
-const Car = {str:"BMW"};
-const someObj = {str: "Salary"};
-console.info("My first car is", Car,". I bought it with my first",someObj,);
+// const Car = {str:"BMW"};
+// const someObj = {str: "Salary"};
+// console.info("My first car is", Car,". I bought it with my first",someObj,);
 
 // console.time("answer time");
 // alert("Click to Continue");
 // console.timelog("answer time");
 // alert("Do a bunch of other stuffs...");
 // console.timeEnd("answer time");
+
+console.log("This is the outer level");
+console.group("First group");
+console.log("In the first group");
+console.group("Second group");
+console.log("In the second group");
+console.warn("Still in the second group");
+console.groupEnd();
+console.log("Back to the first group");
+console.groupEnd();
+console.debug("Back to the outer level");
